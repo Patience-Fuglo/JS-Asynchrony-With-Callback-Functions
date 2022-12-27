a58830140dea88f15c797e0b9d680bd83314c2ac
Patience-Fuglo <pfuglo@fordham.edu>
2022-12-27 01:36:30 -0500

    complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
+
 npc.walkNorth(1400, () => {
     npc.walkEast(1200, () => {
         npc.walkSouth(300, () => {
@@ -18,6 +19,8 @@ npc.walkNorth(1400, () => {
         })
     })
 })
+
+
 
 // Create the inventory
 const inventory = newInventory()
